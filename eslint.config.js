6cd235d7b94a029aa52be442490d9acbf2fe2864
BehramBakhtKhan <behrambakhtkhan22@gmail.com>
2026-09-11 00:29:05 +0500

    fix: sync linting and prettier configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-const js = require("@eslint/js");
+const js = require('@eslint/js');
 
 module.exports = [
   js.configs.recommended,
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs",
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
       globals: {
-        module: "readonly",
-        require: "readonly",
-        process: "readonly",
+        module: 'readonly',
+        require: 'readonly',
+        process: 'readonly',
       },
     },
   },
