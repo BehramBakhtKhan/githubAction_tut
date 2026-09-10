aba2618c9b843043324141514222c80df2074180
BehramBakhtKhan <behrambakhtkhan22@gmail.com>
2026-09-10 23:45:16 +0500

    added a branch feature/multiply and added aligned code

--- math.test.js
+++ math.test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const test = require("node:test");
-const { sum, subtract } = require("./math");
+const { sum, subtract, multiply } = require("./math");
 
 test("adds 1 + 2 to equal 3", () => {
   assert.strictEqual(sum(1, 2), 3);
@@ -9,3 +9,7 @@ test("adds 1 + 2 to equal 3", () => {
 test("subtracts 5 - 2 to equal 3", () => {
   assert.strictEqual(subtract(5, 2), 3);
 });
+
+test("multiply 2 * 2 to equal 4", () => {
+  assert.strictEqual(multiply(2,2), 4)
+})
